4fc877aafdcb91150d1b3935a7acab79d7f6da0a
Luis Brito <luiseduardo14@gmail.com>
2018-12-10 12:01:05 -0200

    Fix: Controller generator options

--- generators/controller/index.js
+++ generators/controller/index.js
@@ -1,6 +1,9 @@
 'use strict';
+const Path = require('path');
 const Generator = require('yeoman-generator');
 const slugify = require('underscore.string/slugify');
+
+const BASE_PATH = './api/controllers';
 
 const toPascalCase = (input) => {
   return input.match(/[a-z]+/gi)
@@ -18,34 +21,38 @@ module.exports = class extends Generator {
     this.argument('name', {
       type: String,
       required: true,
+      description: 'The name of the entity'
+    });
+
+    // Adds support for custom path
+    this.option('path', {
+      type: String,
+      required: false,
+      default: BASE_PATH,
+      description: 'Specify a custom path for script generation'
+    });
+
+    // Adds support for custom path
+    this.option('base-url', {
+      type: String,
+      required: false,
+      description: 'Specify the controller base URL for requests'
     });
 
     // Prepare context utils
     this.slugify = slugify;
   }
 
-  prompting() {
-    const prompts = [{
-      type: 'input',
-      name: 'basePath',
-      message: 'What\'s the controller base path?',
-      default: `/${slugify(this.options.name)}`
-    }];
-
-    return this.prompt(prompts).then(props => {
-      this.options.basePath = props.basePath;
-    });
-  }
-
   writing() {
     const ctrlName = `${toPascalCase(this.options.name)}Controller`;
 
-    // Copy Docker files
     this.fs.copyTpl(
       this.templatePath('BaseController.ts.ejs'),
-      this.destinationPath(`./api/controllers/${ctrlName}.ts`), {
+      this.destinationPath(
+        Path.resolve(process.cwd(), this.options.path, `${ctrlName}.ts`)
+      ), {
         ctrlName,
-        basePath: this.options.basePath,
+        baseUrl: this.options.baseUrl || `/${slugify(this.options.name)}`,
       }, {
         globOptions: {
           extension: false,
